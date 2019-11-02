1db8cee088cb88340a18ff754e4258b810bf1737
DanPerry1808 <danperry1808@gmail.com>
2019-11-02 16:51:31 +0000

    [WIP] Endpoint Collision again
    - Removed the current connection for endpoint collision triggering the level change.

--- End point/EndPoint.gd
+++ End point/EndPoint.gd
@@ -3,10 +3,7 @@ extends RigidBody2D
 class_name EndPoint
 
 export(PackedScene) var next_scene
-onready var coll_rect = $CollisionShape2D
 
 func change_level():
 	get_tree().change_scene(next_scene)
 
-func _on_RigidBody2D_body_entered(body):
-	change_level()
