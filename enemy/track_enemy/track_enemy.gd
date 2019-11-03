49e6e908307a7f9bc2070057d6e6813678ffcb7b
adszads <51888045+adszads@users.noreply.github.com>
2019-11-03 11:15:01 +0000

    tracks now working on stage 4

--- enemy/track_enemy/track_enemy.gd
+++ enemy/track_enemy/track_enemy.gd
@@ -26,7 +26,8 @@ func _physics_process(delta):
 		var track
 		for area in areas:
 			track = area.get_owner()
-			if track.IS_TRACK and track != last_track:
+			if track.get('IS_TRACK') != null and track != last_track:
+				print('aaa')
 				found_track = true
 				move(track)
 
