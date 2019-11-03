aad37b9b58ddd9dd01399e6f552857b52ced7f09
Paul Deshmukh-Reeves <pdeshmukh-reeves1@sheffield.ac.uk>
2019-11-03 08:55:29 +0000

    bullets and batteries
    fixed batteries not being picked up
    fixed batteries not being able to hit the player

--- bullet/bullet.gd
+++ bullet/bullet.gd
@@ -5,9 +5,12 @@ class_name Bullet
 const IS_ENEMY = true
 
 func _on_bullet_body_enter(body):
-	if body.has_method("hit"):
-		body.hit(1)
-		get_parent().remove_child(self)
+	if body.get("IS_ENEMY") != null:
+		if body.IS_ENEMY:
+			body.hit(1)
+		else:
+			body.update_energy(-25)
+	get_parent().remove_child(self)
 
 func _on_Timer_timeout():
 	($Anim as AnimationPlayer).play("shutdown")
